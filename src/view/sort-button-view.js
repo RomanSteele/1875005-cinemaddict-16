fbd0e6625a545a8dc49da44f889d15fe7230f2cc
RomanSteele <romanzelenenko@yandex.ru>
2021-12-26 16:44:18 +0300

    module5-task2 vol.3

--- src/view/sort-button-view.js
+++ src/view/sort-button-view.js
@@ -8,7 +8,7 @@ const createSortButtonsTemplate = (sortType) => {
 
   return (
     `<ul class="sort">
-    <li><a href="#" class="sort__button ${defaultClass}" data-sort-type="${SortType.DEFAULT}">Sort by default</a></li>
+    <li><a href="#" class="sort__button sort__button--active ${defaultClass}" data-sort-type="${SortType.DEFAULT}">Sort by default</a></li>
     <li><a href="#" class="sort__button ${dateClass}" data-sort-type="${SortType.DATE}">Sort by date</a></li>
     <li><a href="#" class="sort__button ${ratingClass}" data-sort-type="${SortType.RATING}">Sort by rating</a></li>
   </ul>`
