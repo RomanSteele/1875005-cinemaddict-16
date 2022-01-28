e062a74d09c2fb2cad0adf374cf0833e7f46b123
RomanSteele <romanzelenenko@yandex.ru>
2022-01-28 19:09:34 +0300

    .

--- src/view/films-view.js
+++ src/view/films-view.js
@@ -2,7 +2,6 @@ import AbstractView from './abstract-view.js';
 
 const createFilmsTemplate = () => '<section class="films"></section>';
 
-
 export default class FilmsView extends AbstractView{
 
   get template() {
